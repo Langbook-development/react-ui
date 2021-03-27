413de9c8111bd8f4f0ad803427d898a5dee3c48f
Vladislav <vladislav.ykshn@gmail.com>
2021-03-27 21:53:56 +0300

    Reworked NoteNavigationItemDraggable.js

--- src/components/note_navigation/NoteNavigationItemDraggable.js
+++ src/components/note_navigation/NoteNavigationItemDraggable.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function NoteNavigationItemDraggable(props) {
   const { noteId, level, width } = props;
   return (
-    <div style={{ width }}>
+    <div style={{ width }} className="navigation-item-draggable">
       <NoteNavigationItem noteId={noteId} level={level} />
     </div>
   );
