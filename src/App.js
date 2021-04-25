aa2730f8d7b5968014cfafcad08fec45bebd2f52
Vladislav <vladislav.ykshn@gmail.com>
2021-04-25 16:06:17 +0300

    Redirect logic using ifst

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { useEffect } from "react";
 import NotePage from "./pages/notes/NotePage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NoteEmptyPage from "./pages/notes/NoteEmptyPage";
 import { useDispatch, useSelector } from "react-redux";
 import { getNotes } from "./state/notes/thunks";
 import { CustomDragLayer } from "./pages/notes/navigation/drag_utils/CustomDragLayer";
-import { isNotesLoadedSelector } from "./state/notes/selectors";
 
 function App() {
   const dispatch = useDispatch();
-  const isNotesLoaded = useSelector(isNotesLoadedSelector);
   useEffect(() => {
     dispatch(getNotes());
   }, [dispatch]);
@@ -20,10 +17,11 @@ function App() {
         <header />
         <Switch>
           <Route exact path="/">
-            <NoteEmptyPage />
+            <NotePage />
+            <CustomDragLayer />
           </Route>
           <Route path="/notes/:selectedNoteId">
-            {isNotesLoaded ? <NotePage /> : <NoteEmptyPage />}
+            <NotePage />
             <CustomDragLayer />
           </Route>
         </Switch>
--- src/pages/notes/NotePage.js
+++ src/pages/notes/NotePage.js
@@ -2,14 +2,24 @@ import React, { useEffect } from "react";
 import NoteNavigation from "./navigation/NoteNavigation";
 import NoteSection from "./content/NoteSection";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useParams } from "react-router-dom";
-import { noteExistsSelector } from "../../state/notes/selectors";
+import { Redirect, useParams, useLocation } from "react-router-dom";
+import {
+  firstToShowNoteSelector,
+  hasNotesSelector,
+  isNotesLoadedSelector,
+  noteExistsSelector,
+} from "../../state/notes/selectors";
 import { noteExpanded } from "../../state/notes/notesSlice";
 
 function NotePage() {
+  const { pathname } = useLocation();
   const { selectedNoteId } = useParams();
+  const isNotesLoaded = useSelector(isNotesLoadedSelector);
+  const hasNotes = useSelector(hasNotesSelector);
   const noteExists = useSelector(noteExistsSelector(selectedNoteId));
+  const firstToShowNoteId = useSelector(firstToShowNoteSelector);
   const dispatch = useDispatch();
+  const hasLoadedNotes = isNotesLoaded && hasNotes;
 
   useEffect(() => {
     if (noteExists) {
@@ -17,13 +27,19 @@ function NotePage() {
     }
   }, []);
 
-  if (!noteExists) {
-    return <Redirect to="/" />;
+  if (pathname === "/") {
+    if (isNotesLoaded && hasNotes) {
+      return <Redirect to={"/notes/" + firstToShowNoteId} />;
+    }
+  } else {
+    if (isNotesLoaded && (!noteExists || !hasNotes)) {
+      return <Redirect to="/" />;
+    }
   }
   return (
     <main className="note-page">
       <NoteNavigation />
-      <NoteSection />
+      {hasLoadedNotes ? <NoteSection /> : <div />}
     </main>
   );
 }
